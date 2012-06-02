b540481649deb613a820c04fbfe45cdc0342b4a5
zhou jin gao <haliluya0000@hotmail.com>
2012-06-01 23:15:03 -0400

    depot code generated by rails new command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '17cd8917cca10629b17c615482a4f32ff9103bbca41d31f45710f0ff9a2f69caa6248e6cfc6399dd868adb88b7fce95f6726c0800bffa34d33b240a117a52540'
